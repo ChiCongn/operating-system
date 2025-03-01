a637d1bb04516cff5b66af1419b50df79e26682f
ChiCongn <chicongvn1205@gmail.com>
2025-03-01 17:14:09 +0700

    update: added process name and created constructors for easier process creation

--- src/main/java/scheduling/models/Process.java
+++ src/main/java/scheduling/models/Process.java
@@ -2,16 +2,19 @@ package scheduling.models;
 
 public class Process {
     int id;
+    String name;
     int arrivalTime;
     int burstTime;
     int remainingTime;
     int priority;
+    int queueLevel;
     int startTime = -1;
-    int completionTime;
+    int finishTime;
     int turnaroundTime;
     int waitingTime;
     int responseTime;
     boolean isCompleted = false;
+
 
     public Process(int id, int arrivalTime, int burstTime, int priority) {
         this.id = id;
@@ -21,24 +24,154 @@ public class Process {
         this.priority = priority;
     }
 
+    // for First Come First Served
+    public Process(String name, int arrivalTime, int burstTime) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+    }
+
+    // For priority number
+    public Process(String name, int arrivalTime, int burstTime, int priority) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.priority = priority;
+    }
+
+    // for multi-level (response) queue scheduling
+    public Process(String name, int arrivalTime, int burstTime, int priority, int queueLevel) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.remainingTime = burstTime;
+        this.priority = priority;
+        this.queueLevel = queueLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "Process{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", arrivalTime=" + arrivalTime +
+                ", burstTime=" + burstTime +
+                ", remainingTime=" + remainingTime +
+                ", priority=" + priority +
+                ", queueLevel=" + queueLevel +
+                ", startTime=" + startTime +
+                ", completionTime=" + finishTime +
+                ", turnaroundTime=" + turnaroundTime +
+                ", waitingTime=" + waitingTime +
+                ", responseTime=" + responseTime +
+                ", isCompleted=" + isCompleted +
+                '}';
+    }
+
     // Getters and Setters
-    public int getId() { return id; }
-    public int getArrivalTime() { return arrivalTime; }
-    public int getBurstTime() { return burstTime; }
-    public int getRemainingTime() { return remainingTime; }
-    public void setRemainingTime(int remainingTime) { this.remainingTime = remainingTime; }
-    public int getPriority() { return priority; }
-    public int getStartTime() { return startTime; }
-    public void setStartTime(int startTime) { this.startTime = startTime; }
-    public int getCompletionTime() { return completionTime; }
-    public void setCompletionTime(int completionTime) { this.completionTime = completionTime; }
-    public int getTurnaroundTime() { return turnaroundTime; }
-    public void setTurnaroundTime(int turnaroundTime) { this.turnaroundTime = turnaroundTime; }
-    public int getWaitingTime() { return waitingTime; }
-    public void setWaitingTime(int waitingTime) { this.waitingTime = waitingTime; }
-    public int getResponseTime() { return responseTime; }
-    public void setResponseTime(int responseTime) { this.responseTime = responseTime; }
-    public boolean isCompleted() { return isCompleted; }
-    public void setCompleted(boolean completed) { isCompleted = completed; }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+
+    public void setRemainingTime(int remainingTime) {
+        this.remainingTime = remainingTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getQueueLevel() {
+        return queueLevel;
+    }
+
+    public void setQueueLevel(int queueLevel) {
+        this.queueLevel = queueLevel;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(int finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public int getTurnaroundTime() {
+        return turnaroundTime;
+    }
+
+    public void setTurnaroundTime(int turnaroundTime) {
+        this.turnaroundTime = turnaroundTime;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    public int getResponseTime() {
+        return responseTime;
+    }
+
+    public void setResponseTime(int responseTime) {
+        this.responseTime = responseTime;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
 }
 
