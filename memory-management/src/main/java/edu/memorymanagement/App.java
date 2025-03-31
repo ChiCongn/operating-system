ac716f3219b80030210c40c12b3b4df1c51cde7b
ChiCongn <chicongvn1205@gmail.com>
2025-03-31 15:21:09 +0700

    update: run app

--- memory-management/src/main/java/edu/memorymanagement/App.java
+++ memory-management/src/main/java/edu/memorymanagement/App.java
@@ -1,4 +1,39 @@
 package edu.memorymanagement;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        //SceneManager.setPrimaryStage(primaryStage);
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource("/edu/memorymanagement/paging.fxml"));
+            AnchorPane root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("App");
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
